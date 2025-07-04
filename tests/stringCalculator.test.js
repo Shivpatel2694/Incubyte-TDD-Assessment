5082369f7c25fac7598e11e1f40b5f6d941dd9f8
unknown <shivpatel2694@gmail.com>
2025-07-04 19:53:36 +0530

    [STEP 1][RED] Added failing test-Case for single input

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -2,7 +2,12 @@ import{describe,it,expect} from 'vitest';
 import { add } from '../src/stringCalculator'
 
 describe('String Calculator - KATA',()=>{
+
     it('return 0 for empty string',()=>{
         expect(add("")).toBe(0)
+    })
+
+    it("return the number itself incase of single input",()=>{
+        expect(add(1).toBe(1))
     })
 })
