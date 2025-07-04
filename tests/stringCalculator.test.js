431a099fb73c8cbaf315c4f9f82e0a76f62558f4
unknown <shivpatel2694@gmail.com>
2025-07-04 19:42:07 +0530

    [STEP 1][RED] Added failing test for empty string

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -1,8 +1,8 @@
 import{describe,it,expect} from 'vitest';
-import { dryRun } from '../src/stringCalculator'
+import { add } from '../src/stringCalculator'
 
-describe('dryRun',()=>{
-    it('dryRun for demo',()=>{
-        expect(dryRun()).toBe()
+describe('String Calculator - KATA',()=>{
+    it('return 0 for empty string',()=>{
+        expect(add("")).toBe(0)
     })
 })
