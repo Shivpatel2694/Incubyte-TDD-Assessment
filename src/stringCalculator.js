dcd09b2626d90f0f863583202d75bd25d259b987
unknown <shivpatel2694@gmail.com>
2025-07-04 22:14:50 +0530

    [STEP 1][GREEN] Implementation of addition of 2 numbers separated by commas

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,5 +3,12 @@ export  function add (inputString){
     //return 0; just passing the test
     if(inputString == "" || inputString == null)  return 0 ;
     
-    if(inputString.length == 1) return Number(inputString);//refactoring
+    if(inputString.length == 1) return Number(inputString);
+
+    const parts = inputString.split(",");
+    const a = Number(parts[0]);
+    const b = Number(parts[1]);
+
+    return a + b;
+    //refactoring
 };
