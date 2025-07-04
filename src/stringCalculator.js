7659db9a6d6943ab0a4f24b9342b7f8ab643f51a
unknown <shivpatel2694@gmail.com>
2025-07-04 23:20:46 +0530

    [STEP 2][GREEN] Implementation of handling any amount of numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,14 +1,23 @@
-export  function add (inputString){
-    
-    //return 0; just passing the test
-    if(inputString == "" || inputString == null)  return 0 ;
-    
-    if(inputString.length == 1) return Number(inputString);
+export function add(inputString) {
+  //handles empty array
+  if (inputString == "" || inputString == null) return 0;
 
-    const parts = inputString.split(",");
-    const a = Number(parts[0]);
-    const b = Number(parts[1]);
+  const parts = inputString.split(",");
 
+  //handles single input
+  if (parts.length == 1) return Number(parts[0].trim());
+  //handles input with 2 numbers
+  else if (parts.length == 2) {
+    const a = Number(parts[0].trim());
+    const b = Number(parts[1].trim());
     return a + b;
-    //refactoring
-};
+  }
+  //handles any amount of numbers
+  else {
+    let sum = 0;
+    for (let i = 0; i < parts.length; i++) {
+      sum += Number(parts[i].trim());
+    }
+    return sum;
+  }
+}
