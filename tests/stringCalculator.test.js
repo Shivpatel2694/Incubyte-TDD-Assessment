59a18324df33be226a0816ab30c538d9d6844e9a
unknown <shivpatel2694@gmail.com>
2025-07-05 01:45:03 +0530

    [STEP 4][RED] Added the failing test case for handling custom delimiter

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -20,7 +20,11 @@ describe('String Calculator - KATA',()=>{
     })
 
     it("should return the sum of number while handling the new lines between numbers",()=>{
-        expect(add("1\n2,3")).toBe(6)
+        expect(add("1,2\n,3")).toBe(6)
+    })
+
+    it("should return the sum while handling the custom delimiters",()=>{
+        expect(add("//;\n1;2")).toBe(3);
     })
 
 })
