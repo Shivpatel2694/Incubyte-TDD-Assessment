34e8b8976780c84c1e3809c83b501f4d3ca841b9
unknown <shivpatel2694@gmail.com>
2025-07-04 23:42:33 +0530

    [STEP 2][REFACTOR] Enhanced the implementation of handling any amount of numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -6,18 +6,12 @@ export function add(inputString) {
 
   //handles single input
   if (parts.length == 1) return Number(parts[0].trim());
-  //handles input with 2 numbers
-  else if (parts.length == 2) {
-    const a = Number(parts[0].trim());
-    const b = Number(parts[1].trim());
-    return a + b;
-  }
+
   //handles any amount of numbers
-  else {
     let sum = 0;
     for (let i = 0; i < parts.length; i++) {
       sum += Number(parts[i].trim());
     }
     return sum;
   }
-}
+
