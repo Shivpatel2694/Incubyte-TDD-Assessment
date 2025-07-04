660205a6c84337cc091c59122a45a95c5bf435b5
unknown <shivpatel2694@gmail.com>
2025-07-05 02:06:40 +0530

    [STEP 4][GREEN] Implementation of handling the custom delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,8 +2,16 @@ export function add(inputString) {
   //handles empty array
   if (inputString == "" || inputString == null) return 0;
 
-  //handles new line delimiters between numbers
-  const parts = inputString.split(/[\n,]/);
+  let delimiter = /[\n,]/;
+  
+  //handleling custom delimiters
+  if (inputString.startsWith('//')) {
+    const customDelimiter = inputString[2]; 
+    delimiter = new RegExp(customDelimiter); 
+    inputString = inputString.slice(4);
+  }
+
+   const parts = inputString.split(new RegExp(delimiter));
 
   //handles single input
   if (parts.length == 1) return Number(parts[0].trim());
