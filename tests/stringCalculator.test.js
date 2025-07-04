6e633d35e80b393a4e9efec43c3492928302f66c
unknown <shivpatel2694@gmail.com>
2025-07-04 18:41:59 +0530

    [INIT] Set up Node and installed vitest

---
+++ tests/stringCalculator.test.js
@@ -0,0 +1,8 @@
+import{describe,it,expect} from 'vitest';
+import { dryRun } from '../src/stringCalculator'
+
+describe('dryRun',()=>{
+    it('dryRun for demo',()=>{
+        expect(dryRun()).toBe()
+    })
+})
