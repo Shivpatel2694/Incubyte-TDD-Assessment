1915f1b70e0e1b79fe9fe67d6f0aa3c60d79ce77
unknown <shivpatel2694@gmail.com>
2025-07-05 16:50:21 +0530

    [STEP 8][GREEN] Implementation of handling  multiple single character delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,43 +3,46 @@ export function add(inputString) {
   if (inputString == "" || inputString == null) return 0;
 
   let delimiter = /[\n,]/;
-  
+
   //handleling custom delimiters
-  if (inputString.startsWith('//')) {
+  if (inputString.startsWith("//")) {
     let delimiterSectionEnd = inputString.indexOf("\n");
     const delimiterSection = inputString.slice(2, delimiterSectionEnd);
-    
-    const match = delimiterSection.match(/^\[(.+)\]$/);
 
-    const customDelimiter = match ? match[1] : delimiterSection;
-    const escaped = customDelimiter.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    delimiter = new RegExp(escaped);
-    
-     inputString = inputString.slice(delimiterSectionEnd + 1);
+    const matches = delimiterSection.match(/\[([^\]]+)\]/g);
+
+    if (matches) {
+      const escaped = matches
+        .map((d) => d.slice(1, -1))
+        .map((c) => c.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))
+        .join("|");
+      delimiter = new RegExp(escaped);
+    } else {
+      const escaped = delimiterSection.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+      delimiter = new RegExp(escaped);
+    }
+
+    inputString = inputString.slice(delimiterSectionEnd + 1);
   }
 
   //custom delimitor used to split
 
-   const parts = inputString.split(delimiter);
+  const parts = inputString.split(delimiter);
 
   //handles any amount of numbers
-    let sum = 0;
-    let negatives =[];
-    for (let i = 0; i < parts.length; i++) {
-    
-      let num = Number(parts[i].trim());
+  let sum = 0;
+  let negatives = [];
+  for (let i = 0; i < parts.length; i++) {
+    let num = Number(parts[i].trim());
 
-      if(num < 0)
-        negatives.push(num);
+    if (num < 0) negatives.push(num);
 
-      if(num > 1000)
-        continue;
+    if (num > 1000) continue;
 
-      sum+= num;
-    }
-
-    if(negatives.length > 0)
-        throw new Error(`Negative numbers are not allowed: ${negatives.join(",")}`);
-    return sum;
+    sum += num;
   }
 
+  if (negatives.length > 0)
+    throw new Error(`Negative numbers are not allowed: ${negatives.join(",")}`);
+  return sum;
+}
